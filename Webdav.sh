b204e6d47494e2042cfb5c1775115078b572370e
Tawakkal23 <55172998+Tawakkal23@users.noreply.github.com>
2019-09-13 10:59:07 +0800

    Add files via upload

---
+++ Webdav.sh
@@ -0,0 +1,31 @@
+#!/system/bin/sh
+clear
+figlet "WEBDAV                 " | lolcat
+echo "#####@@@-_Tawakkal23_-@@@#####  " | lolcat
+echo
+echo "# Gunakan Tool ini Sebaik baiknya" | lolcat
+echo "# Jangan Ceroboh" | lolcat
+echo
+echo "---------------------------------" | lolcat
+echo "=> Sedang Menginstall Webdav....." | lolcat
+echo "---------------------------------" | lolcat
+echo
+sleep 7
+cd $HOME
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+apt install openssl curl && apt install libcurl
+mkdir webdav
+cd webdav
+apt install wget
+wget http://pastebin.com/raw/HnVyQPtR
+mv HnVyQPtR webdav.py
+chmod 777 webdav.py
+
+clear
+figlet " Terinstall " | lolcat
+echo
+sleep 3
+echo "> Webdav Sudah Terinstall" | lolcat
+echo "> Silahkan Cek Di HOME TermuxMu" | lolcat
+echo
